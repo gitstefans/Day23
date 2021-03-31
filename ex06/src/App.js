6d873dfe11e559a4122307747aa96c089fb2293a
stefan <stefansgit@gmail.com>
2021-03-31 02:28:53 +0200

    Submitting Form Data fix

--- ex06/src/App.js
+++ ex06/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
         })
     }
     handleSubmit = (character) => {
-        this.setState({ characters: [...this.state.characters, character] })
+        this.setState({ characters: [...this.state.characters, character] });
     }
     render() { 
         const { characters } = this.state;
